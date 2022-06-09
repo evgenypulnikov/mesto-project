e17ec7f3d2f455dd5ae66fe3096e904cb33ae677
Evgeny Pulnikov <sklve52@outlook.com>
2022-06-09 06:47:20 +0300

    fix: refactoring

--- src/index.js
+++ src/index.js
@@ -11,24 +11,40 @@ import { validationParams,
   enableValidation
 } from '../src/modules/validate.js';
 
-import { editProfileButton,
+import { popups,
+  editProfileButton,
   editProfilePopup,
   profileNameElement,
   profileStatusElement,
   editProfileForm,
   profileNameInput,
   profileStatusInput,
+  editProfileSubmit,
   addPlaceButton,
   addPlacePopup,
   addPlaceForm,
   placeTitleInput,
   placeUrlInput,
+  addPlaceSubmit,
   placesContainer
 } from '../src/modules/vars.js';
 
 /*___ Render Cards */
 
 renderAllCards();
+
+/*___ Modals Listener */
+
+popups.forEach((popup) => {
+  popup.addEventListener('mousedown', (evt) => {
+    if (evt.target.classList.contains('popup_is_opened')) {
+      closePopup(popup);
+    }
+    if (evt.target.classList.contains('popup__close-button')) {
+      closePopup(popup)
+    }
+  });
+});
 
 /*___ Edit Profile Modal Listeners */
 
@@ -45,6 +61,9 @@ editProfileForm.addEventListener('submit', function(evt) {
   profileNameElement.textContent = profileNameInput.value;
   profileStatusElement.textContent = profileStatusInput.value;
 
+  editProfileSubmit.classList.add('form__submit_is_disabled');
+  editProfileSubmit.setAttribute('disabled', '');
+
   closePopup(editProfilePopup);
 });
 
@@ -59,6 +78,9 @@ addPlaceForm.addEventListener('submit', function(evt) {
 
   const card = createCard(placeUrlInput.value, placeTitleInput.value);
   renderCard(card, placesContainer);
+
+  addPlaceSubmit.classList.add('form__submit_is_disabled');
+  addPlaceSubmit.setAttribute('disabled', '');
 
   addPlaceForm.reset();
   closePopup(addPlacePopup);
