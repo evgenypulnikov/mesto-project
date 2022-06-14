2a94dc0e6fb2ead21498f679087acb87e3b5fa31
Evgeny Pulnikov <sklve52@outlook.com>
2022-06-14 18:34:27 +0300

    feat: json api

--- src/modules/vars.js
+++ src/modules/vars.js
@@ -1,9 +1,18 @@
 const popups = document.querySelectorAll('.popup');
 
+/*___ Change Avatar Modal */
+
+const changeAvatarButton = document.querySelector('.profile__avatar-change-button');
+const changeAvatarPopup = document.querySelector('.popup_avatar-change');
+const changeAvatarForm = document.forms.avatarChangeForm;
+const changeAvatarUrlInput = changeAvatarForm.elements.avatarUrl;
+const changeAvatarSubmit = changeAvatarForm.querySelector('.form__submit');
+
 /*___ Edit Profile Modal */
 
 const editProfileButton = document.querySelector('.profile__edit-button');
 const editProfilePopup = document.querySelector('.popup_edit-profile');
+const profileAvatarElement = document.querySelector('.profile__picture');
 const profileNameElement = document.querySelector('.profile__name');
 const profileStatusElement = document.querySelector('.profile__status');
 const editProfileForm = document.forms.editProfileForm;
@@ -21,35 +30,6 @@ const placeUrlInput = addPlaceForm.elements.placeUrl;
 const addPlaceSubmit = addPlaceForm.querySelector('.form__submit');
 const placesContainer = document.querySelector('.photo-grid__list');
 
-/*___ Init Places */
-
-const initPlaces = [
-  {
-    name: 'Карачаево-Черкесск',
-    link: 'https://images.unsplash.com/photo-1634665598022-325f0152926e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://images.unsplash.com/photo-1535427284698-c8e68a1eb910?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1168&q=80'
-  },
-  {
-    name: 'Дагестан',
-    link: 'https://images.unsplash.com/photo-1633975029239-83e696ffab0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=385&q=80'
-  },
-  {
-    name: 'Северная Осетия',
-    link: 'https://images.unsplash.com/photo-1612719734814-73ed4b4235e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
-  },
-  {
-    name: 'Златоуст',
-    link: 'https://images.unsplash.com/photo-1637073665858-44e5f12d3917?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80'
-  },
-  {
-    name: 'Роза Хутор',
-    link: 'https://images.unsplash.com/photo-1632414612752-f7619ec1d2c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
-  }
-]
-
 /*___ Full View */
 
 const fullViewPopup = document.querySelector('.popup_image-full');
@@ -57,8 +37,14 @@ const fullViewImg = fullViewPopup.querySelector('.full-view__image');
 const fullViewImgCaption = fullViewPopup.querySelector('.full-view__caption');
 
 export { popups,
+  changeAvatarButton,
+  changeAvatarPopup,
+  changeAvatarForm,
+  changeAvatarUrlInput,
+  changeAvatarSubmit,
   editProfileButton,
   editProfilePopup,
+  profileAvatarElement,
   profileNameElement,
   profileStatusElement,
   editProfileForm,
@@ -72,7 +58,6 @@ export { popups,
   placeUrlInput,
   addPlaceSubmit,
   placesContainer,
-  initPlaces,
   fullViewPopup,
   fullViewImg,
   fullViewImgCaption
